2fb7cc02aa6b62e189bcb20e5161c8918827ccda
Bradley Parker <bparker@Bradleys-MacBook-Air.local>
2024-08-13 16:34:15 -0500

    Initial commit

---
+++ src/main/java/com/bradley/spring_api_example/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.bradley.spring_api_example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello, Bradley!";
+    }
+}
